facba0a88c1fdcdf6231d7ee15bd4ddc76e80f7a
Waco <waco@Wacos-MacBook-Pro.local>
2023-01-20 19:10:13 +1000

    finished card layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    }
+    ,extend: {},
   },
   plugins: [],
 }
