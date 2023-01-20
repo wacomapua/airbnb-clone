616efdcf9e792f0afe64d99b94473f45be7b819c
Waco <waco@Wacos-MacBook-Pro.local>
2023-01-20 22:28:11 +1000

    added data folder, responsive design, reusable component

---
+++ src/components/data.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const data = [
+  {
+    id: 1,
+    img: '/katie.png',
+    title: 'Life lessons with Katie Zaferes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, eget aliquam nisl nisl sit amet lorem.',
+    price: 136,
+    img: '/katie.png',
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    location: 'USA',
+    openSpots: 3,
+  },
+  {
+    id: 2,
+    img: '/wedding-photography.png',
+    title: 'Learn wedding photography',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, eget aliquam nisl nisl sit amet lorem.',
+    price: 125,
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+    location: 'Online',
+    openSpots: 21,
+  },
+  {
+    id: 3,
+    img: '/mountain-bike.png',
+    title: 'Group Mountain Biking',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, eget aliquam nisl nisl sit amet lorem.',
+    price: 50,
+    stats: {
+      rating: 4.8,
+      reviewCount: 8,
+    },
+    location: 'Madrid',
+    openSpots: 7,
+  },
+];
+export default data;
